52c42f243479ec798bd70cd07ee1dcb97711f6d1
facundo <salcedocristianit@gmail.com>
2023-06-26 17:51:07 -0300

    p3

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,35 +1,140 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 
-// int a;
-// int b;
+bool realizarCalculo = true;
 
-// a = 10;
-// b=a;
-
-// Console.WriteLine("valor de a:"+a);
-// Console.WriteLine("valor de b:"+b);
-
-Console.WriteLine("Ingrese un número:");
-string input = Console.ReadLine();
-if (int.TryParse(input, out int number) && number > 0)
+while (realizarCalculo)
 {
-    int reversedNumber = ReverseNumber(number);
-    Console.WriteLine("El número invertido es: " + reversedNumber);
-}
-else
-{
-    Console.WriteLine("El texto ingresado no es un número válido o el número es menor o igual a 0.");
-}
+    Console.WriteLine("Calculadora");
+    Console.WriteLine("1. Sumar");
+    Console.WriteLine("2. Restar");
+    Console.WriteLine("3. Multiplicar");
+    Console.WriteLine("4. Dividir");
+    Console.WriteLine("5. Valor absoluto");
+    Console.WriteLine("6. Cuadrado");
+    Console.WriteLine("7. Raíz cuadrada");
+    Console.WriteLine("8. Seno");
+    Console.WriteLine("9. Coseno");
+    Console.WriteLine("10. Parte entera de un float");
+    Console.WriteLine("11. Máximo entre dos números");
+    Console.WriteLine("12. Mínimo entre dos números");
 
-static int ReverseNumber(int number)
-{
-    int reversedNumber = 0;
-    while (number != 0)
+    Console.WriteLine("Ingrese la opción deseada:");
+    string opcion = Console.ReadLine();
+
+    if (opcion == "5" || opcion == "6" || opcion == "7" || opcion == "8" || opcion == "10")
     {
-        int remainder = number % 10;
-        reversedNumber = reversedNumber * 10 + remainder;
-        number /= 10;
+        Console.WriteLine("Ingrese un número:");
+        double numero = double.Parse(Console.ReadLine());
+        double resultado = 0;
+
+        switch (opcion)
+        {
+            case "5":
+                resultado = Math.Abs(numero);
+                Console.WriteLine("El valor absoluto del número es: " + resultado);
+                break;
+            case "6":
+                resultado = Math.Pow(numero, 2);
+                Console.WriteLine("El cuadrado del número es: " + resultado);
+                break;
+            case "7":
+                if (numero >= 0)
+                {
+                    resultado = Math.Sqrt(numero);
+                    Console.WriteLine("La raíz cuadrada del número es: " + resultado);
+                }
+                else
+                {
+                    Console.WriteLine("No se puede calcular la raíz cuadrada de un número negativo.");
+                }
+                break;
+            case "8":
+                resultado = Math.Sin(numero);
+                Console.WriteLine("El seno del número es: " + resultado);
+                break;
+            case "10":
+                resultado = Math.Floor(numero);
+                Console.WriteLine("La parte entera del número es: " + resultado);
+                break;
+            default:
+                Console.WriteLine("Opción inválida.");
+                break;
+        }
     }
-    return reversedNumber;
+    else if (opcion == "11" || opcion == "12")
+    {
+        Console.WriteLine("Ingrese el primer número:");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo número:");
+        double num2 = double.Parse(Console.ReadLine());
+
+        double resultado = 0;
+
+        switch (opcion)
+        {
+            case "11":
+                resultado = Math.Max(num1, num2);
+                Console.WriteLine("El máximo entre los dos números es: " + resultado);
+                break;
+            case "12":
+                resultado = Math.Min(num1, num2);
+                Console.WriteLine("El mínimo entre los dos números es: " + resultado);
+                break;
+            default:
+                Console.WriteLine("Opción inválida.");
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Ingrese el primer número:");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo número:");
+        double num2 = double.Parse(Console.ReadLine());
+
+        double resultado = 0;
+
+        switch (opcion)
+        {
+            case "1":
+                resultado = num1 + num2;
+                Console.WriteLine("El resultado de la suma es: " + resultado);
+                break;
+            case "2":
+                resultado = num1 - num2;
+                Console.WriteLine("El resultado de la resta es: " + resultado);
+                break;
+            case "3":
+                resultado = num1 * num2;
+                Console.WriteLine("El resultado de la multiplicación es: " + resultado);
+                break;
+            case "4":
+                if (num2 != 0)
+                {
+                    resultado = num1 / num2;
+                    Console.WriteLine("El resultado de la división es: " + resultado);
+                }
+                else
+                {
+                    Console.WriteLine("No se puede dividir entre 0.");
+                }
+                break;
+            default:
+                Console.WriteLine("Opción inválida.");
+                break;
+        }
+    }
+
+    Console.WriteLine("¿Desea realizar otro cálculo? (s/n)");
+    string respuesta = Console.ReadLine();
+
+    if (respuesta.ToLower() != "s")
+    {
+        realizarCalculo = false;
+    }
+
+    Console.WriteLine();
 }
