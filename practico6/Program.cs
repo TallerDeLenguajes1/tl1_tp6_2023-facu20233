2b972b6910e86c9d89ba2e5a923499e31fb0bde4
facundo <salcedocristianit@gmail.com>
2023-06-26 16:43:51 -0300

    p1

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,12 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 
-int a;
-int b;
+// int a;
+// int b;
 
-a = 10;
-b=a;
+// a = 10;
+// b=a;
 
-Console.WriteLine("valor de a:"+a);
-Console.WriteLine("valor de b:"+b);
+// Console.WriteLine("valor de a:"+a);
+// Console.WriteLine("valor de b:"+b);
 
+Console.WriteLine("Ingrese un número:");
+string input = Console.ReadLine();
+if (int.TryParse(input, out int number) && number > 0)
+{
+    int reversedNumber = ReverseNumber(number);
+    Console.WriteLine("El número invertido es: " + reversedNumber);
+}
+else
+{
+    Console.WriteLine("El texto ingresado no es un número válido o el número es menor o igual a 0.");
+}
+
+static int ReverseNumber(int number)
+{
+    int reversedNumber = 0;
+    while (number != 0)
+    {
+        int remainder = number % 10;
+        reversedNumber = reversedNumber * 10 + remainder;
+        number /= 10;
+    }
+    return reversedNumber;
+}
